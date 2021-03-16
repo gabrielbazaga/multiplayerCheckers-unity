151bf989e0af75c38ad9046c426fef30548912ab
gabrielbazaga <gabrielbazaga@gmail.com>
2021-03-15 22:31:25 -0300

    Some bug moves fixes

--- Assets/Scripts/CheckersBoard.cs
+++ Assets/Scripts/CheckersBoard.cs
@@ -11,6 +11,8 @@ public class CheckersBoard : MonoBehaviour
     private Vector3 boardOffset = new Vector3(-4.0f, 0,-4.0f);
     private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
 
+    private bool isWhite;
+    private bool isWhiteTurn;
 
     private Piece selectedPiece;
 
@@ -22,29 +24,51 @@ public class CheckersBoard : MonoBehaviour
 
     private void Start()
     {
+        isWhiteTurn = true;
         GenerateBoard();
     }
 
     private void Update()
     {
+        UpdateMouseOver();
 
-        if (selectedPiece != null)
-            UpdatePieceDrag(selectedPiece);
         // if it is my turn
         {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+
+            if (selectedPiece != null)
+                UpdatePieceDrag(selectedPiece);
+
             if (Input.GetMouseButtonDown(0))
-                SelectPiece((int)mouseOver.x, (int)mouseOver.y);
+                SelectPiece(x, y);
 
             if (Input.GetMouseButtonUp(0))
-                TryMove((int)startDrag.x, (int)startDrag.y, (int)mouseOver.x, (int)mouseOver.y);
+                TryMove((int)startDrag.x, (int)startDrag.y, x, y);
+
+            Debug.Log("startdrag X: "+ (int)startDrag.x+ "startdrag Y: " + (int)startDrag.y + "       mouseover x: " + x + "mouseover Y: " + y);
         }
     }
-
     private void UpdateMouseOver()
     {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
 
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
     }
-
     private void UpdatePieceDrag(Piece p)
     {
         if (!Camera.main)
@@ -59,11 +83,11 @@ public class CheckersBoard : MonoBehaviour
             p.transform.position = hit.point + Vector3.up;
         }
 
-    }
 
+    }
     private void SelectPiece(int x, int y)
     {
-        //out of bounds
+        // out of bounds
         if(x < 0 || x >= maxBoardSquare || y < 0 || y >= maxBoardSquare)
         {
             return;
@@ -78,9 +102,9 @@ public class CheckersBoard : MonoBehaviour
         }
     }
 
-    private void TryMove(int x1, int y1, int x2, int y2)
+    public void TryMove(int x1, int y1, int x2, int y2)
     {
-        //multiplayer support
+        // multiplayer support
         startDrag = new Vector2(x1, y1);
         endDrag = new Vector2(x2, y2);
         selectedPiece = pieces[x1, y1];
@@ -109,13 +133,13 @@ public class CheckersBoard : MonoBehaviour
             }
 
             // check if its a valid move
-            if(selectedPiece.ValidMove(pieces, x1, y1,x2,y2))
+            if(selectedPiece.ValidMove(pieces, x1, y1, x2, y2))
             {
                 //did we kill anything
                 //if this is a jump
                 if(Mathf.Abs(x2-x2) == 2)
                 {
-                    Piece p = pieces[(x1 + x2) / 2, (y1 - y2) / 2];
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
                     if(p != null)
                     {
                         pieces[(x1 + x2) / 2, (y1 - y2) / 2] = null;
@@ -134,9 +158,17 @@ public class CheckersBoard : MonoBehaviour
 
     private void EndTurn()
     {
+        selectedPiece = null;
+        startDrag = Vector2.zero;
 
+        isWhiteTurn = !isWhiteTurn;
+        CheckVictory();
     }
 
+    private void CheckVictory()
+    {
+
+    }
     private void GenerateBoard()
     {
         // Generate white team
@@ -146,7 +178,7 @@ public class CheckersBoard : MonoBehaviour
             for (int x=0; x<8; x+=2)
             {
                 // Generate pieces
-                GeneratePiece((oddRow)?x:x+1,y);
+                GeneratePiece((oddRow) ? x : x+1, y);
             }
         }
 
@@ -161,7 +193,6 @@ public class CheckersBoard : MonoBehaviour
             }
         }
     }
-
     public void GeneratePiece(int x, int y)
     {
         bool isWhite = (y > 3) ? false : true;
@@ -173,7 +204,6 @@ public class CheckersBoard : MonoBehaviour
         MovePiece(p, x, y);
         
     }
-
     private void MovePiece(Piece p, int x, int y)
     {
         p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -9,12 +9,13 @@ public class Piece : MonoBehaviour
 
     public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2)
     {
-        //if you are moving on top of another piece
+        // if you are moving on top of another piece
         if (board[x2, y2] != null)
             return false;
 
         int deltaMove = Mathf.Abs(x1 - x2);
         int deltaMoveY = Mathf.Abs(y1 - y2);
+
         if (isWhite || isKing)
         {
             if(deltaMove == 1)
@@ -26,7 +27,7 @@ public class Piece : MonoBehaviour
             {
                 if(deltaMoveY == 2)
                 {
-                    Piece p = board[(x1 + x2) / 2, (y1 - y2) / 2];
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
                     if (p != null && p.isWhite != isWhite)
                         return true;
                 }
@@ -44,7 +45,7 @@ public class Piece : MonoBehaviour
             {
                 if (deltaMoveY == -2)
                 {
-                    Piece p = board[(x1 + x2) / 2, (y1 - y2) / 2];
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
                     if (p != null && p.isWhite != isWhite)
                         return true;
                 }
